6033cd25eb92037553b936287f034e110d1be8a2
Shivam Singh Darbar <shivamdarbar04@gmail.com>
2023-07-13 18:37:53 +0530

    Standalone collection in spring

---
+++ test.java
@@ -0,0 +1,23 @@
+package com.springcore.standalone.collections;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class test 
+{
+
+	public static void main(String[] args) 
+	{
+	
+	ApplicationContext con = new ClassPathXmlApplicationContext("com/springcore/standalone/collections/aloneconfig.xml");
+	Person person1 =  con.getBean("person1",Person.class);
+	System.out.println(person1);
+	System.out.println(person1.getFriends().getClass().getName());
+
+	System.out.println("------------------------------------------");
+	System.out.println(person1.getFeestructure());
+	
+	System.out.println("------------------------------------------");
+	System.out.println(person1. getProperties());
+	}
+}
